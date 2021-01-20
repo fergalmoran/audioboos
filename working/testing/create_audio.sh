d7a9f5f1911d0f05d18f9bc72448e865db28413f
Fergal Moran <fergal.moran@gmail.com>
2021-01-20 17:58:52 +0000

    Added audio test script

---
+++ working/testing/create_audio.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+echo Removing existing
+rm -rf albums/
+NUM_ARTISTS=5
+NUM_ALBUMS=5
+NUM_TRACKS=9
+FREQ_START=500
+FREQ_INC=46 #in honour of Joe
+
+for artist in $(seq 1 $NUM_ARTISTS)
+do
+    artist_folder="Artist ${artist}"
+    echo "Creating ${artist_folder}"
+    for album in $(seq 1 $NUM_ALBUMS)
+    do
+        album_folder="${artist_folder}/${artist_folder} - Album ${album}"
+        echo "Creating ${album_folder}"
+        mkdir -p "albums/${album_folder}"
+        for track in $(seq 1 $NUM_TRACKS)
+        do
+            let frequency=$(($FREQ_START+($FREQ_INC * $track)))
+            track_file="albums/${album_folder}/Artist ${artist} - Album ${album} - Track ${track}.wav"
+            echo ${track_file}
+            ffmpeg -f lavfi -i "sine=frequency=${frequency}:duration=30" -ac 2 "albums/${album_folder}/Artist ${artist} - Album ${album} - Track ${track}.wav"
+        done
+    done
+done
+
